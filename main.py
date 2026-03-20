3cbfd96a782bfd493a6bfaeced664ac38f4a16ce
EmilyRajapakse2004 <lidiyarajapakse@gmail.com>
2026-03-20 18:24:39 +0530

    added loggers to the main

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from scripts.etl import clean_transform
 from scripts.load_to_db import load_to_db
 
 def run_pipeline():
-    print("🚀 Starting End-to-End Data Pipeline...\n")
+    print(" Starting End-to-End Data Pipeline...\n")
 
     # Step 1: Ingestion
     df_raw = ingest_data()
 
-    # Step 2: ETL / Transformation
+    # Step 2: ETL
     df_transformed = clean_transform()
 
-    # Step 3: Load to Database
+    # Step 3: Load
     db_path = load_to_db()
 
     print("\n Pipeline completed successfully!")
-    print(f"Data is now available at {db_path}")
+    print(f" Data available at: {db_path}")
 
 if __name__ == "__main__":
     run_pipeline()
