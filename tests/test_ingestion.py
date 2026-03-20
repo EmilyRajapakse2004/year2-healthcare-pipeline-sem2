c97722cf32d4fd4b423b527693608072d27d44fc
EmilyRajapakse2004 <lidiyarajapakse@gmail.com>
2026-03-20 18:24:58 +0530

    added commnets to test_ingestion.py

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -2,7 +2,7 @@ from scripts.ingestion import ingest_data
 
 def test_ingestion():
     df = ingest_data()
-    print("\n🔹 First 5 rows:")
+    print("\n First 5 rows:")
     print(df.head())
 
 if __name__ == "__main__":
