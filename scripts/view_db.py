5e72ca7408ad14638853d803a602e719c8b867cd
EmilyRajapakse2004 <lidiyarajapakse@gmail.com>
2026-03-20 18:25:23 +0530

    added comments to view_db.py

--- scripts/view_db.py
+++ scripts/view_db.py
@@ -1,7 +1,15 @@
 import sqlite3
 import pandas as pd
 
-conn = sqlite3.connect("data/processed/healthcare.db")
-df = pd.read_sql("SELECT * FROM readmission_data LIMIT 5", conn)
-print(df.head())
-conn.close()
+def view_data():
+    conn = sqlite3.connect("data/processed/healthcare.db")
+
+    df = pd.read_sql("SELECT * FROM readmission_data LIMIT 5", conn)
+
+    print(" Sample Data from Database:")
+    print(df.head())
+
+    conn.close()
+
+if __name__ == "__main__":
+    view_data()
