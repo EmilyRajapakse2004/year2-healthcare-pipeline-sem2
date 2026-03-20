8eb06a1539555719d9a32c598de56ecf48445753
EmilyRajapakse2004 <lidiyarajapakse@gmail.com>
2026-03-20 13:01:44 +0530

    Initialize ingestion.py

---
+++ tests/test_ingestion.py
@@ -0,0 +1,9 @@
+from scripts.ingestion import ingest_data
+
+def test_ingestion():
+    df = ingest_data()
+    print("\n🔹 First 5 rows:")
+    print(df.head())
+
+if __name__ == "__main__":
+    test_ingestion()
