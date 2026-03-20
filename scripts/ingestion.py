303396d44c2374027d1c04c86c965a5c58c99c53
EmilyRajapakse2004 <lidiyarajapakse@gmail.com>
2026-03-20 18:23:51 +0530

    added commnets to ingestion.py

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -6,16 +6,14 @@ PROCESSED_DIR = "data/processed/"
 OUTPUT_FILE = "data/processed/ingested_data.csv"
 
 def ingest_data():
-    # Create processed folder if not exists
+    print(" STEP 1: Ingestion started...")
+
     os.makedirs(PROCESSED_DIR, exist_ok=True)
 
-    # Load raw data
     df = pd.read_csv(RAW_PATH)
 
-    print(" Data loaded successfully")
-    print("Shape:", df.shape)
+    print(f" Data loaded. Shape: {df.shape}")
 
-    # Save a copy to processed layer
     df.to_csv(OUTPUT_FILE, index=False)
 
     print(f" Data saved to {OUTPUT_FILE}")
